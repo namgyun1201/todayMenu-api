33f6d4d1a435552e228e76dd3288ba8ac8a968c3
namgyun1201@gmail.com <namgyun1201@gmail.com>
2023-04-22 16:30:04 +0900

    user 테이블 추가

--- database/migrations/2023_04_08_131210_create_pgsql_tables.php
+++ database/migrations/2023_04_08_131210_create_pgsql_tables.php
@@ -10,6 +10,19 @@ return new class () extends Migration {
      */
     public function up(): void
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100)->nullable()->comment('성+이름');
+            $table->string('account')->comment('로그인 계정');
+            $table->string('email', 100)->comment('이메일 주소');
+            $table->string('password')->nullable()->comment('비밀번호');
+            $table->json('mobile')->nullable()->comment('핸드폰번호');
+            $table->timestampsTz($precision = 3);
+            $table->softDeletesTz($column = 'deleted_at', $precision = 3);
+
+            $table->index('id');
+        });
+
         # 재료
         Schema::create('ingredients', function (Blueprint $table) {
             $table->bigIncrements('id');
@@ -66,6 +79,7 @@ return new class () extends Migration {
      */
     public function down(): void
     {
+        Schema::dropIfExists('users');
         Schema::dropIfExists('ingredients');
         Schema::dropIfExists('recipies');
         Schema::dropIfExists('processes');
